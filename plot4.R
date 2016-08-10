8f7288f914bdd5f490986c6494a592d7fab8bfd3
Ashwini <ashwini.pai@telentix.in>
2016-08-10 21:17:24 +0530

    Adding datetime label to plot4.

--- plot4.R
+++ plot4.R
@@ -12,7 +12,7 @@ par(mfrow=c(2,2), mar=c(6,6,1,1))
 #First Plot
 plot(changed$Datetime, changed$Global_active_power, type="l", main="",xlab="",ylab="Global Active Power (killowatts)")
 #Second Plot
-plot(changed$Datetime, changed$Voltage, type="l", main="",xlab="",ylab="Voltage")
+plot(changed$Datetime, changed$Voltage, type="l", main="",xlab="datetime",ylab="Voltage")
 #Third Plot
 plot(changed$Datetime, changed$Sub_metering_1, type="n", main="",xlab="",ylab="Energy sub metering")
 #Drawing the points for each readings
@@ -22,7 +22,7 @@ points(changed$Datetime, changed$Sub_metering_3, col = "blue", type="l")
 #Adding the legends with colors
 legend("topright",col=c("#ABABAB","#FB0007","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),bty="n",lwd=c(1,1),yjust=2)
 #Fourth Plot
-plot(changed$Datetime, changed$Global_reactive_power, type="l", main="",xlab="",ylab="Global_reactive_power")
+plot(changed$Datetime, changed$Global_reactive_power, type="l", main="",xlab="datetime",ylab="Global_reactive_power")
 #Saving to file
 #dev.print(png, file = "plot4.png", width = 480, height = 480)
 dev.off()
